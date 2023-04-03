0407b9ee7f7a9426f167ac1ba5f1fab0a6e83601
Anne Zepecki <amzepecki@gmail.com>
2023-04-02 17:33:31 -0700

    Improve documentation

--- src/fin_libs/dividends/__init__.py
+++ src/fin_libs/dividends/__init__.py
@@ -1,34 +1,34 @@
 """
 Library module to compute dividend information
+
+Typical usage example:
+
+        aapl_dr = calculate_dividend_rate("AAPL")
 """
 import yfinance as yf
 
 
-"""
-Calculate dividend rate
-
-Args:
-        ticker(str): name of the ticker
-Returns:
-        float: dividend rate
-"""
-
-
 def calculate_dividend_rate(ticker):
+    """
+    Calculate dividend rate
+
+    Args:
+            ticker (str): name of the ticker
+    Returns:
+            float: dividend rate
+    """
     tick = yf.Ticker(ticker)
     return tick.info['dividendRate']
 
 
-"""
-Calculate dividend yield
-
-Args:
-        ticker(str): name of the ticker
-Returns:
-        float: dividend yield
-"""
-
-
 def calculate_dividend_yield(ticker):
+    """
+    Calculate dividend yield
+
+    Args:
+            ticker (str): name of the ticker
+    Returns:
+            float: dividend yield
+    """
     tick = yf.Ticker(ticker)
     return tick.info['dividendYield']
